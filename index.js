4dae40c99ef136d6323c9cb39d43d1e60029ae23
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-30 23:56:33 -0300

    add debugging logs

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ var watchDirectory = isLinux ? watchFallback : watchRecursive
 module.exports = watch
 
 function watch (name, onchange) {
+  console.log('watch', name)
+
   var clear = null
   var stopped = false
 
   fs.lstat(name, function (_, st) {
+    console.log('watch -> fs.lstat', name, { stopped })
+
     if (!st || stopped) {
       stopped = true
       return
@@ -32,9 +36,16 @@ function watch (name, onchange) {
 function watchFile (filename, onchange) {
   var prev = null
   var prevTime = 0
+  var cleanup = false
+
+  console.log('watchFile', filename)
 
   var w = fs.watch(filename, function () {
+    console.log('watchFile fs.watch onchange', filename, { cleanup })
+
     fs.lstat(filename, function (_, st) {
+      console.log('watchFile fs.watch -> fs.lstat', filename, { cleanup })
+
       var now = Date.now()
       if (now - prevTime > 2000 || !same(st, prev)) onchange(filename)
       prevTime = now
@@ -43,17 +54,25 @@ function watchFile (filename, onchange) {
   })
 
   return function () {
+    console.log('watchFile cleanup')
+    cleanup = true
     w.close()
   }
 }
 
 function watchRecursive (directory, onchange) {
+  console.log('watchRecursive', directory)
+
+  var cleanup = false
+
   var w = fs.watch(directory, {recursive: true}, function (change, filename) {
+    console.log('watchRecursive fs.watch', { cleanup })
     if (!filename) return // filename not always given (https://nodejs.org/api/fs.html#fs_filename_argument)
     onchange(path.join(directory, filename))
   })
 
   return function () {
+    cleanup = true
     w.close()
   }
 }
@@ -127,11 +146,15 @@ function watchFallback (directory, onchange) {
   function visit (next, cb) {
     var dir = path.join(directory, next)
 
+    console.log('visit fs.lstat', { cleanup })
+
     fs.lstat(dir, function (err, st) {
       if (err || !st.isDirectory()) return cb()
       if (cleanup) return cb()
       if (watching[dir]) return cb()
       if (loaded) emit(dir)
+
+      console.log('visit fs.lstat fs.watch', { cleanup })
 
       var w = fs.watch(dir, function (change, filename) {
         filename = path.join(next, filename)
